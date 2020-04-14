5a6e52344668e4fed8291aabe25ca5cbbe0cd292
Seth <skeror@gmail.com>
2020-04-14 15:50:53 +0800

    Initial Commit

---
+++ 2020class-hw-weight_database/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020class-hw-weight_database
+//
+//  Created by Seth on 2020/4/14.
+//  Copyright © 2020 Seth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
